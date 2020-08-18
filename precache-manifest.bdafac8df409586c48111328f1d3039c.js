280e261e4a8400abc3f63499e611fe87dd9f9e78
Maroš Vida <maros.vida@student.tuke.sk>
2020-08-18 12:27:25 +0200

    Updates

---
+++ precache-manifest.bdafac8df409586c48111328f1d3039c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b49a864bfef1ffec254f80a17ae44ed",
+    "url": "/react-tunes-app/index.html"
+  },
+  {
+    "revision": "04dc18a5dcea6dceb747",
+    "url": "/react-tunes-app/static/css/main.6701eb68.chunk.css"
+  },
+  {
+    "revision": "9de8ba87af2381e24138",
+    "url": "/react-tunes-app/static/js/2.8bc63453.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-tunes-app/static/js/2.8bc63453.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04dc18a5dcea6dceb747",
+    "url": "/react-tunes-app/static/js/main.4d432312.chunk.js"
+  },
+  {
+    "revision": "18dfc7b3ee75954c1b92",
+    "url": "/react-tunes-app/static/js/runtime-main.e02a0fa8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-tunes-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4aa3e4453d9814865da791924a207927",
+    "url": "/react-tunes-app/static/media/search.4aa3e445.svg"
+  }
+]);
